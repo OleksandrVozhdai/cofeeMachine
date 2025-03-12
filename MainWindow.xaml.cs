b4b0f1cd1d02b8f7590c61fdb495920c449a1da4
Vozhdai Oleksandr <oleksander.vozhdai@gmail.com>
2025-03-13 00:37:59 +0200

    Added new grids

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Coffee_Machine
 {
@@ -28,6 +29,8 @@ namespace Coffee_Machine
             Menu.Visibility = Visibility.Visible;
             CofeeSelection.Visibility = Visibility.Hidden;
             CoffeeSelectionPageTwo.Visibility = Visibility.Hidden;
+            CoffeePreparing.Visibility = Visibility.Hidden;
+            CustomDrink.Visibility = Visibility.Hidden;
         }
 
         #region Menu
@@ -111,8 +114,32 @@ namespace Coffee_Machine
 
             EspressoButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
             EspressoButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
-        }
 
+            var drink = new Espresso();
+            ChangeDrinkLabelContent(drink.Name);
+
+            drink.MakeDrink(DrinkProgressBar);
+
+            CoffeePreparing.Visibility = Visibility.Visible;
+
+            DoubleAnimation currentPgeAnim = new DoubleAnimation
+            {
+                From = 0,
+                To = -900,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            DoubleAnimation coffeePrepAnim = new DoubleAnimation
+            {
+                From = 900,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            currentPgeAnim.Completed += currentPgeAnimCompl;
+            CoffeeTransform.BeginAnimation(TranslateTransform.XProperty, currentPgeAnim);
+            CoffeePreparingTransform.BeginAnimation(TranslateTransform.XProperty, coffeePrepAnim);
+        }
 
 
         private void Latte_PreviewMouseDown(object sender, MouseButtonEventArgs e)
@@ -139,6 +166,30 @@ namespace Coffee_Machine
 
             LatteButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
             LatteButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
+
+            var drink = new Latte();
+            ChangeDrinkLabelContent(drink.Name);
+            drink.MakeDrink(DrinkProgressBar);
+
+            CoffeePreparing.Visibility = Visibility.Visible;
+
+            DoubleAnimation currentPgeAnim = new DoubleAnimation
+            {
+                From = 0,
+                To = -900,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            DoubleAnimation coffeePrepAnim = new DoubleAnimation
+            {
+                From = 900,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            currentPgeAnim.Completed += currentPgeAnimCompl;
+            CoffeeTransform.BeginAnimation(TranslateTransform.XProperty, currentPgeAnim);
+            CoffeePreparingTransform.BeginAnimation(TranslateTransform.XProperty, coffeePrepAnim);
         }
 
 
@@ -167,6 +218,30 @@ namespace Coffee_Machine
 
             CappuccinoButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
             CappuccinoButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
+
+            var drink = new Cappuccino();
+            ChangeDrinkLabelContent(drink.Name);
+            drink.MakeDrink(DrinkProgressBar);
+
+            CoffeePreparing.Visibility = Visibility.Visible;
+
+            DoubleAnimation currentPgeAnim = new DoubleAnimation
+            {
+                From = 0,
+                To = -900,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            DoubleAnimation coffeePrepAnim = new DoubleAnimation
+            {
+                From = 900,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            currentPgeAnim.Completed += currentPgeAnimCompl;
+            CoffeeTransform.BeginAnimation(TranslateTransform.XProperty, currentPgeAnim);
+            CoffeePreparingTransform.BeginAnimation(TranslateTransform.XProperty, coffeePrepAnim);
         }
 
 
@@ -226,7 +301,7 @@ namespace Coffee_Machine
         #endregion
 
         #region chooseCofeeTwo
-        private void Macchiato_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        private void Macchiato_PreviewMouseDown(object sender, MouseButtonEventArgs e) //bug, this is chocolate
         {
             DoubleAnimation scaleAnim = new DoubleAnimation
             {
@@ -239,7 +314,7 @@ namespace Coffee_Machine
             MacchiatoButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
         }
 
-        private void Macchiato_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        private void Macchiato_PreviewMouseUp(object sender, MouseButtonEventArgs e) //bug, this is chocolate
         {
             DoubleAnimation scaleAnim = new DoubleAnimation
             {
@@ -251,39 +326,86 @@ namespace Coffee_Machine
             MacchiatoButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
             MacchiatoButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
 
-            CoffeeSelectionPageTwo.Visibility = Visibility.Visible;
+            var drink = new Macchiato();
+            ChangeDrinkLabelContent(drink.Name);
+            drink.MakeDrink(DrinkProgressBar);
+
+            CoffeePreparing.Visibility = Visibility.Visible;
+
+            DoubleAnimation currentPgeAnim = new DoubleAnimation
+            {
+                From = 0,
+                To = -900,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            DoubleAnimation coffeePrepAnim = new DoubleAnimation
+            {
+                From = 900,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            currentPgeAnim.Completed += currentPgeAnimCompl;
+            CoffeeTransformPageTwo.BeginAnimation(TranslateTransform.XProperty, currentPgeAnim);
+            CoffeePreparingTransform.BeginAnimation(TranslateTransform.XProperty, coffeePrepAnim);
+        }
+
+        private void Chocolate_PreviewMouseDown(object sender, MouseButtonEventArgs e) //bug, this is Macchiato
+        {
+            DoubleAnimation scaleAnim = new DoubleAnimation
+            {
+                From = 1,
+                To = 0.9,
+                Duration = TimeSpan.FromSeconds(0.1)
+            };
+
+            ChocolateButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
+            ChocolateButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
+        }
+
+        private void Chocolate_PreviewMouseUp(object sender, MouseButtonEventArgs e) //bug, this is Macchiato
+        {
+            DoubleAnimation scaleAnim = new DoubleAnimation
+            {
+                From = 0.9,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.1)
+            };
+
+            ChocolateButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
+            ChocolateButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
+
+            var drink = new Chocolate();
+            ChangeDrinkLabelContent(drink.Name);
+            drink.MakeDrink(DrinkProgressBar);
+
+            CoffeePreparing.Visibility = Visibility.Visible;
+
+            DoubleAnimation currentPgeAnim = new DoubleAnimation
+            {
+                From = 0,
+                To = -900,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            DoubleAnimation coffeePrepAnim = new DoubleAnimation
+            {
+                From = 900,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            currentPgeAnim.Completed += currentPgeAnimCompl;
+            CoffeeTransformPageTwo.BeginAnimation(TranslateTransform.XProperty, currentPgeAnim);
+            CoffeePreparingTransform.BeginAnimation(TranslateTransform.XProperty, coffeePrepAnim);
+        }
+
+        private void currentPgeAnimCompl(object sender, EventArgs e)
+        {
             CofeeSelection.Visibility = Visibility.Hidden;
+            CoffeeSelectionPageTwo.Visibility = Visibility.Hidden;
         }
-
-   
-
-        private void Chocolate_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            DoubleAnimation scaleAnim = new DoubleAnimation
-            {
-                From = 1,
-                To = 0.9,
-                Duration = TimeSpan.FromSeconds(0.1)
-            };
-
-            ChocolateButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
-            ChocolateButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
-        }
-
-        private void Chocolate_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            DoubleAnimation scaleAnim = new DoubleAnimation
-            {
-                From = 0.9,
-                To = 1,
-                Duration = TimeSpan.FromSeconds(0.1)
-            };
-
-            ChocolateButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
-            ChocolateButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
-        }
-
-
 
         private void AddMore_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -309,8 +431,27 @@ namespace Coffee_Machine
 
             AddMoreButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
             AddMoreButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
-        }
 
+            CustomDrink.Visibility = Visibility.Visible;
+
+            DoubleAnimation currentPgeAnim = new DoubleAnimation
+            {
+                From = 0,
+                To = -900,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            DoubleAnimation customDrinkAnim = new DoubleAnimation
+            {
+                From = 900,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            currentPgeAnim.Completed += currentPgeAnimCompl;
+            CoffeeTransformPageTwo.BeginAnimation(TranslateTransform.XProperty, currentPgeAnim);
+            CustomDrinkTransform.BeginAnimation(TranslateTransform.XProperty, customDrinkAnim);
+        }
 
 
         private void PageOne_PreviewMouseDown(object sender, MouseButtonEventArgs e)
@@ -362,8 +503,165 @@ namespace Coffee_Machine
         private void PageOneAnimationCompleted(object sender, EventArgs e)
         {
             CoffeeSelectionPageTwo.Visibility = Visibility.Hidden;
-
         }
         #endregion
+
+        #region CupPage
+
+        private void CupButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DoubleAnimation scaleAnim = new DoubleAnimation
+            {
+                From = 1,
+                To = 0.9,
+                Duration = TimeSpan.FromSeconds(0.1)
+            };
+
+            CupButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
+            CupButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
+        }
+
+        private void CupButton_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            DoubleAnimation scaleAnim = new DoubleAnimation
+            {
+                From = 0.9,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.1)
+            };
+
+            CupButtonScale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
+            CupButtonScale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
+
+
+        }
+
+        #endregion
+
+        #region Logic
+
+        abstract class Drink
+        {
+            public string Name { get; set; }
+            public TimeSpan TimetoPrepare { get; }
+            public decimal Cost { get; }
+
+            public Drink(string name, TimeSpan time, decimal cost)
+            {
+                this.Name = name;
+                this.TimetoPrepare = time;
+                this.Cost = cost;
+            }
+
+            public abstract void MakeDrink(ProgressBar progressBar);
+        }
+
+
+        class Espresso : Drink
+        {
+            public Espresso() : base("Espresso", TimeSpan.FromSeconds(10), 3.00m) { }
+
+            public override async void MakeDrink(ProgressBar progressBar)
+            {
+                progressBar.Value = 1;
+
+                int steps = 100;
+                int delay = (int)(TimetoPrepare.TotalMilliseconds / steps);
+
+                for (int i = 1; i <= steps; i++)
+                {
+                    await Task.Delay(delay);
+                    progressBar.Value = i;
+                }
+            }
+        }
+
+        class Latte : Drink
+        {
+            public Latte() : base("Latte", TimeSpan.FromSeconds(15), 4.00m) { }
+
+            public override async void MakeDrink(ProgressBar progressBar)
+            {
+                progressBar.Value = 1;
+
+                int steps = 100;
+                int delay = (int)(TimetoPrepare.TotalMilliseconds / steps);
+
+                for (int i = 1; i <= steps; i++)
+                {
+                    await Task.Delay(delay);
+                    progressBar.Value = i;
+                }
+            }
+        }
+
+        class Cappuccino : Drink
+        {
+            public Cappuccino() : base("Cappuccino", TimeSpan.FromSeconds(10), 3.00m) { }
+
+            public override async void MakeDrink(ProgressBar progressBar)
+            {
+                progressBar.Value = 1;
+
+                int steps = 100;
+                int delay = (int)(TimetoPrepare.TotalMilliseconds / steps);
+
+                for (int i = 1; i <= steps; i++)
+                {
+                    await Task.Delay(delay);
+                    progressBar.Value = i;
+                }
+            }
+        }
+
+        class Macchiato : Drink
+        {
+            public Macchiato() : base("Macchiato", TimeSpan.FromSeconds(10), 4.00m) { }
+
+            public override async void MakeDrink(ProgressBar progressBar)
+            {
+                progressBar.Value = 1;
+
+                int steps = 100;
+                int delay = (int)(TimetoPrepare.TotalMilliseconds / steps);
+
+                for (int i = 1; i <= steps; i++)
+                {
+                    await Task.Delay(delay);
+                    progressBar.Value = i;
+                }
+            }
+        }
+
+        class Chocolate : Drink
+        {
+            public Chocolate() : base("Chocolate", TimeSpan.FromSeconds(10), 3.00m) { }
+
+            public override async void MakeDrink(ProgressBar progressBar)
+            {
+                progressBar.Value = 1;
+
+                int steps = 100;
+                int delay = (int)(TimetoPrepare.TotalMilliseconds / steps);
+
+                for (int i = 1; i <= steps; i++)
+                {
+                    await Task.Delay(delay);
+                    progressBar.Value = i;
+                }
+            }
+        }
+
+        #endregion
+
+        #region other methods
+
+        public void ChangeDrinkLabelContent(string name)
+        {
+            DrinkNameLabel.Content = "Your " + name + " is being prepared...";
+        }
+
+        #endregion
+
     }
 }
